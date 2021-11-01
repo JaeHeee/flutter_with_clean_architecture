dbfff88996947938fee818502ed9f046b44f49a3
JaeHeee <kjha2142@gmail.com>
2021-11-01 23:54:05 +0900

    feat: Add null safety

--- lib/features/number_trivia/domain/repositories/number_trivia_repository.dart
+++ lib/features/number_trivia/domain/repositories/number_trivia_repository.dart
@@ -4,6 +4,6 @@ import '../../../../core/error/failures.dart';
 import '../entities/number_trivia.dart';
 
 abstract class NumberTriviaRepository {
-  Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(int? number);
-  Future<Either<Failure, NumberTrivia>> getRandomNumberTrivia();
+  Future<Either<Failure, NumberTrivia>>? getConcreteNumberTrivia(int number);
+  Future<Either<Failure, NumberTrivia>>? getRandomNumberTrivia();
 }
