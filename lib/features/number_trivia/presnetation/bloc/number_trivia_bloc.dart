ba00d3555a014587bcf9f423450e8f3ed98ca7db
JaeHeee <kjha2142@gmail.com>
2021-11-07 23:10:06 +0900

    feat: Add GetTriviaForConcreteNumber to NumberTriviaBloc

--- lib/features/number_trivia/presnetation/bloc/number_trivia_bloc.dart
+++ lib/features/number_trivia/presnetation/bloc/number_trivia_bloc.dart
@@ -1,16 +1,43 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:flutter_with_clean_architecture/features/number_trivia/domain/entities/number_trivia.dart';
+
+import '../../../../core/util/input_converter.dart';
+import '../../domain/entities/number_trivia.dart';
+import '../../domain/usecases/get_concrete_number_trivia.dart';
+import '../../domain/usecases/get_random_number_trivia.dart';
 
 part 'number_trivia_event.dart';
 part 'number_trivia_state.dart';
 
+const String serverFailureMessage = 'Server Failure';
+const String cacheFailureMessage = 'Cache Failure';
+const String invalidInputFailureMessage =
+    'Invalid Input - The number must be a positive integer or zero.';
+
 class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
-  NumberTriviaBloc(NumberTriviaState initialState) : super(initialState);
-  // NumberTriviaBloc() : super(NumberTriviaInitial())
-  //  {
-  //   on<NumberTriviaEvent>((event, emit) {
-  //     // TODO: implement event handler
-  //   });
-  // }
+  final GetConcreteNumberTrivia concrete;
+  final GetRandomNumberTrivia random;
+  final InputConverter inputConverter;
+
+  NumberTriviaState get initialState => Empty();
+
+  NumberTriviaBloc({
+    required this.concrete,
+    required this.random,
+    required this.inputConverter,
+  }) : super(Empty()) {
+    on<GetTriviaForConcreteNumber>(
+      (event, emit) async {
+        final inputEither =
+            inputConverter.stringToUnsignedInteger(event.numberString);
+
+        inputEither.fold(
+          (failure) {
+            emit(const Error(message: invalidInputFailureMessage));
+          },
+          (integer) => throw UnimplementedError(),
+        );
+      },
+    );
+  }
 }
