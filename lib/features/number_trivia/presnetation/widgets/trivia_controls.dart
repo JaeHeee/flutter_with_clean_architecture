dd0b30faa571bc54ee681ff42d5f1c01072c14b7
JaeHeee <kjha2142@gmail.com>
2021-11-10 23:29:14 +0900

    feat: Add widgets

---
+++ lib/features/number_trivia/presnetation/widgets/trivia_controls.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../bloc/number_trivia_bloc.dart';
+
+class TriviaControls extends StatefulWidget {
+  const TriviaControls({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<TriviaControls> createState() => _TriviaControlsState();
+}
+
+class _TriviaControlsState extends State<TriviaControls> {
+  final controller = TextEditingController();
+  late String inputString;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TextField(
+          controller: controller,
+          keyboardType: TextInputType.number,
+          decoration: const InputDecoration(
+            border: OutlineInputBorder(),
+            hintText: 'Input a number',
+          ),
+          onChanged: (value) {
+            inputString = value;
+          },
+          onSubmitted: (_) {
+            addConcrete();
+          },
+        ),
+        const SizedBox(height: 10),
+        Row(
+          children: [
+            Expanded(
+              child: ElevatedButton(
+                child: const Text('Search'),
+                onPressed: addConcrete,
+              ),
+            ),
+            const SizedBox(width: 10),
+            Expanded(
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.grey.shade500,
+                ),
+                child: const Text('Get random trivia'),
+                onPressed: addRandom,
+              ),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+
+  void addConcrete() {
+    controller.clear();
+    BlocProvider.of<NumberTriviaBloc>(context)
+        .add(GetTriviaForConcreteNumber(inputString));
+  }
+
+  void addRandom() {
+    controller.clear();
+    BlocProvider.of<NumberTriviaBloc>(context)
+        .add(GetTriviaForConcreteNumber(inputString));
+  }
+}
